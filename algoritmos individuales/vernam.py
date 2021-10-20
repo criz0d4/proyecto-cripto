39beafe0ad29940deaf5b2e913b71df04219de25
criz0d4 <crishadez@hotmail.com>
2021-10-20 08:31:58 -0500

    segunda prueba

--- algoritmos individuales/vernam.py
+++ algoritmos individuales/vernam.py
@@ -1,26 +1,27 @@
-#! /usr/bin/python
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
 
+import os
+import binascii
 
 print ("que está pasando")
 
-a='12345'
-b='concatenar'
 
-print(b*len(a))
+def generar_clave(mCla):
 
-def cifrar(clave, datos):
+    key =  binascii.hexlify(os.urandom(len(mCla))).decode()
+    print ('Clave: ' + key)
 
-    n = len(datos)
+    key_binario = ''.join(bin(ord(x))[2:].zfill(8) for x in key)	# Se pasa la clave a binario y se guarda como string
+    print ('Clave en binario: ' + key_binario)
 
-    if len(clave) != n:
-        print("Error: la longitud de clave y los datos no coincide.")
-        return
-    
-    resultado = []
+def cifrar(mCla):
 
-    for i in range(0, n):
-        resultado.append(clave[i] ^ datos[i])
+	#mensaje_binario = ''.join(bin(ord(x))[2:].zfill(8) for x in mCla)	# Se pasa el mensaje a binario y se guarda como string
+	mensaje_binario = binascii(mCla).decode()
+	print ('Mensaje original en binario: ' + mensaje_binario)
 
-    return resultado
 
-cifrar('contraseña','texto')
+generar_clave('astro')
+
+cifrar('astro')
